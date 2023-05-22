388a26bef9e0e451f78661ec637b4f0cabf7ef54
Roxana Fernández <roxana.jacquelinefa@gmail.com>
2023-05-21 23:46:50 -0400

    configuraciones en app-routing.module, app.module y en hojas de estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReservarMesaComponent } from './components/reservar-mesa/reservar-mesa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'reservarMesa', component: ReservarMesaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReservarMesaComponent } from './components/reservar-mesa/reservar-mesa.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select'; 
+import { MatInputModule } from '@angular/material/input'; 
+import { MatDatepickerModule } from '@angular/material/datepicker'; 
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { RestauranteService } from './services/restaurante.service';
+import { ClienteService } from './services/cliente.service';
+import { MesaService } from './services/mesa.service';
+import { ReservaService } from './services/reserva.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservarMesaComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RestauranteService, ClienteService, MesaService, ReservaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
