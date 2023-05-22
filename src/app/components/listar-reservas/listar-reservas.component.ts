292d2efa099e2dba04e78a5e54d983627696c0b8
Roxana Fernández <roxana.jacquelinefa@gmail.com>
2023-05-22 14:55:32 -0400

    Se creó template para añadir funcionalidad de filtro

--- src/app/components/listar-reservas/listar-reservas.component.ts
+++ src/app/components/listar-reservas/listar-reservas.component.ts
@@ -1,10 +1,13 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Reserva } from 'src/app/models/Reserva';
+import { Restaurante } from 'src/app/models/Restaurante';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { MesaService } from 'src/app/services/mesa.service';
 import { ReservaService } from 'src/app/services/reserva.service';
 import { RestauranteService } from 'src/app/services/restaurante.service';
+import { forkJoin } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listar-reservas',
@@ -13,6 +16,9 @@ import { RestauranteService } from 'src/app/services/restaurante.service';
 })
 export class ListarReservasComponent {
   listaReservas: Reserva[] = [];
+  listaRestaurantes: Restaurante[] = [];
+  idRestaurante?: number;
+  selectedFecha?: Date;
   
   constructor(
     private servicioRestaurantes: RestauranteService, 
@@ -21,6 +27,20 @@ export class ListarReservasComponent {
     private servicioReservas: ReservaService) { }
 
   ngOnInit(): void {
+    this.getReservas();
+    this.getRestaurantes();
+  }
+
+  getRestaurantes() {
+    this.servicioRestaurantes.getRestaurantes().subscribe({
+      next: (entity) => {
+        this.listaRestaurantes = entity.datos;
+      },
+      error: (e) => console.log(e)
+    });
+  }
+
+  getReservas(idRestaurante?: number | undefined, fechaReserva?: Date | undefined){
     this.servicioReservas.getReservas().subscribe({
       next: (entity) => {
         this.listaReservas= entity.datos;
@@ -41,12 +61,28 @@ export class ListarReservasComponent {
                 next: (entity) => {
                   item.restaurante = entity.datos;
                   console.log(item.restaurante)
+                  /*if (idRestaurante == undefined && fechaReserva == undefined){
+                    console.log("push reserva");
+                    this.listaReservas.push(reserva);
+                    console.log("LISTA FILTRADA",this.listaReservas);
+                  }
+                  else {
+                    if (idRestaurante != undefined){
+                      console.log("idRestaurante está definido")
+                      if (item.restaurante?.idRestaurante == idRestaurante){
+                        this.listaReservas.push(reserva);
+                      }
+                    }
+                    else{
+                      console.log("idRestaurante es undefined");
+                    }
+                  }*/
                 },
                 error: (e) => console.log(e)
               });
             },
             error: (e) => console.log(e)
-          });          
+          });
         }
       },
       error: (e) => console.log(e)
@@ -56,7 +92,11 @@ export class ListarReservasComponent {
   transformarHora(hora: string){
     let date = new Date(hora);
     date.setHours(date.getHours() + 4);
-    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'});
+  }
+
+  filtrarReservas(){
+    
   }
 
 }
