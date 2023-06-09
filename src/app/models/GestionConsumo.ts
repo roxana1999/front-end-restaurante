d321dba2a480412735db80fd38b2c904049aa44c
Roxana Fernández <roxana.jacquelinefa@gmail.com>
2023-06-09 13:04:15 -0400

    Se modificó GestiónConsumo.ts

--- src/app/models/GestionConsumo.ts
+++ src/app/models/GestionConsumo.ts
@@ -1,16 +1,52 @@
+import { Cliente } from "./Cliente"
+
 export class GestionConsumo {
-    cabecera?: {
-        idMesa: number
-        idCliente: number
-        estado: string
-        total: number
-        fechaCreacion: string
-        horaCreacion: string
-        fechaCierre: string
-        horaCierre: string
+    cabecera?: Cabecera
+    detalles: Detalle[] = []
+}
+
+export class Cabecera {
+    idMesa?: number
+    idCliente?: number
+    estado?: string
+    total?: number
+    fechaCreacion?: string
+    horaCreacion?: string
+    fechaCierre?: string
+    horaCierre?: string
+}
+
+export class Detalle {
+    idProducto: number
+    cantidad: number
+    nombreProducto?: string
+    idCabecera?: number
+    totalCabecera?: number
+    idDetalle?: number
+    constructor(idProducto: number, cantidad: number) {
+        this.idProducto = idProducto
+        this.cantidad = cantidad
     }
-    detalles?: {
-        idProducto: number
-        cantidad: number
-    }
+}
+
+export class DatosDetalle {
+    estado?: string
+    mensaje?: string
+    datos?: Detalle
+}
+
+export class DatosConsumicionMesa {
+    estado?: string
+    mensaje?: string
+    datos: EstadoConsumicion[] = []
+}
+
+export class EstadoConsumicion {
+    clientes?: Cliente
+    detalles?: Detalle[] = []
+    estado?: string
+    fechaHoraFinConsumo?: string
+    fechaHoraInicioConsumo?: string
+    idCabecera?: number
+    total?: number
 }
